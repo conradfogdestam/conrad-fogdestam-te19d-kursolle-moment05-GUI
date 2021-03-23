d0d5963bb8e824f3e4b9c93a5ec8e89a2e15f2d1
conradfogdestam <70263566+conradfogdestam@users.noreply.github.com>
2021-03-23 14:51:10 +0100

    doink

--- GUI_windows.py
+++ GUI_windows.py
@@ -1,17 +1,27 @@
 import PySimpleGUI as sg
+import functions
 
 def loginwindow():
     sg.theme('DarkPurple7')
     layout = [[sg.Text('Please enter credentials')],
             [sg.Text('Username', size=(15, 1)), sg.InputText()],
             [sg.Text('Password', size=(15, 1)), sg.InputText(password_char='*')],
-            [sg.Button('Back'), sg.Submit('No account? Register here')],
+            [sg.Button('Back'), sg.Submit('No account? Register here'), sg.Button('Login')],
             [sg.Text('© 2021 CAYMAN ISLANDS NATIONAL BANK™ CAY IS, Inc. All rights reserved.', justification='center', size=(62, 1), font='Helvetica 10')]]
 
     loginpage = sg.Window('CAYMAN ISLANDS NATIONAL BANK™', layout)
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = loginpage.read()
+        if event == 'Login':
+            Username = values[0]
+            Password = values[1]
+            if functions.login == True:
+                loginwindow.close()
+                logged_in_window()
+            else:
+                print('Entered wrong password or username')
+
         if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
             break
         if event == 'Back':
@@ -20,7 +30,8 @@ def loginwindow():
         if event == 'No account? Register here':
             loginpage.close()
             registerwindow()
-        print('You entered ', values[0])
+        print('username ', values[0])
+        print('pass ', values[1])
 
     loginpage.close()
 
@@ -40,6 +51,7 @@ def registerwindow():
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = registerpage.read()
+        
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
         if event == 'Back':
@@ -77,7 +89,7 @@ def startwindow():
     startpage.close()
 
 
-def logged_in():
+def logged_in_window():
     sg.theme('DarkPurple7')
     layout = [[sg.Text('CAYMAN ISLANDS NATIONAL BANK™', text_color='Magenta', size=(31, 1), font='Helvetica 20')],
             [sg.Text('We value your privacy, since 1998', text_color='Magenta', justification='center', size=(62, 1), font='Helvetica 10')],
