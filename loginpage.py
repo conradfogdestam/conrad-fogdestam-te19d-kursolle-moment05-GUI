5f5c7b82798a6305fb5646167b59c4a143c77fe1
conradfogdestam <70263566+conradfogdestam@users.noreply.github.com>
2021-03-23 11:32:03 +0100

    gui start

--- loginpage.py
+++ loginpage.py
@@ -1,9 +1,9 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('CAYMAN ISLANDS NATIONAL BANK™')],
-        [sg.Button('Login','center', size=(20, 2))],
-        [sg.Button('Register', size=(20, 2))] + [sg.Stretch()],
-        [sg.Cancel()]]
+layout = [[sg.Text('Rename files or folders')],
+          [sg.Text('Username', size=(15, 1)), sg.InputText()],
+          [sg.Text('Password', size=(15, 1)), sg.InputText()],
+          [sg.Submit(), sg.Cancel()]]
 
 window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
